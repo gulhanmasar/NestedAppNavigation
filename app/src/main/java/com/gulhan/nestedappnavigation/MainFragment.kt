5e34d74b8095b88302a2b41329f68d4702343f28
gulhan <msrglhn@gmail.com>
2022-09-10 20:10:42 +0300

    [gulhan masar] nested app navigation created for week3

---
+++ app/src/main/java/com/gulhan/nestedappnavigation/MainFragment.kt
@@ -0,0 +1,41 @@
+package com.gulhan.nestedappnavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.gulhan.nestedappnavigation.databinding.FragmentLoginBinding
+import com.gulhan.nestedappnavigation.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+
+    private lateinit var fragmentMainBinding : FragmentMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        fragmentMainBinding = FragmentMainBinding.inflate(inflater)
+        return fragmentMainBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fragmentMainBinding.apply {
+            mainButton.setOnClickListener{
+
+            }
+        }
+    }
+
+}
